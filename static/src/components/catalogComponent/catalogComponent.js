52be30b15840989148cdd21b269798b2d6f4812f
Esteban <acevedoesteban999@gmail.com>
2025-11-13 12:18:05 -0500

    .

--- static/src/components/catalogComponent/catalogComponent.js
+++ static/src/components/catalogComponent/catalogComponent.js
@@ -39,6 +39,35 @@ export class CatalogComponent extends Component {
           scrollbar: { el: '.swiper-scrollbar' },
         });
 
+        this.swiper_products = new Swiper('.swiper-products', {
+          loop: this.state.products.length > 1,
+            speed: 400,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next-products',
+                prevEl: '.swiper-button-prev-products',
+            },
+            pagination: {
+                el: '.swiper-pagination-products',
+                clickable: true,
+            },
+            watchOverflow: true,
+            observer: true,
+            observeParents: true,
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 1,
+                }
+            }
+        });
+        
       });
 
     }
@@ -49,13 +78,25 @@ export class CatalogComponent extends Component {
         [],
         ['name','id']
       )
-      // this.state.products = await this.orm.searchRead(
-      //   'product.template',
-      //   [('product_tmpl_sublimation_id','!=',false)],
-      //   ['name','id','product_childs_sublimation_ids']
-      // )
+      this.state.products = await this.orm.searchRead(
+        'product.template',
+        [['product_tmpl_sublimation_id','=',false]],  
+        ['name','id','product_childs_sublimation_ids']
+      )
        
     }
+
+    getRandomChildren(products, count = 3) {
+      if (!products || products.length === 0) {
+          return Array(count).fill(null);
+      }
+      const shuffled = [...products].sort(() => 0.5 - Math.random());
+      const selected = shuffled.slice(0, Math.min(count, products.length));
+      while (selected.length < count) {
+          selected.push(null);
+      }
+      return selected;
+  }
 }
 
 registry.category("public_components").add("e_sublimation.catalogComponent", CatalogComponent);
