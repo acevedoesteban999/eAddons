86cf5582fd387da1b2f7d2a293cff2c82acff5e3
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-22 01:07:26 -0500

    .

--- e_design_pos/static/src/components/pos_order_line.js
+++ e_design_pos/static/src/components/pos_order_line.js
@@ -2,14 +2,19 @@ import { patch } from "@web/core/utils/patch";
 import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
 
 patch(PosOrderline.prototype,{
+    setup(vals){
+        super.setup(vals)
+    },
     can_be_merged_with(orderline){
-        return super.can_be_merged_with(orderline) && (orderline.design_id?.id === this.raw.design_id?.id )
+        return super.can_be_merged_with(orderline) && (orderline.design_id?.id === this.design_id?.id )
     },
     getDisplayData() {
         let data = super.getDisplayData()
         return {
             ...data,
-            design_id: this.raw.design_id?{'name':this.raw.design_id.name}:false
+            design_id: this.design_id?{
+                default_code: this.design_id.default_code
+            }:false
         }
     }
 })
