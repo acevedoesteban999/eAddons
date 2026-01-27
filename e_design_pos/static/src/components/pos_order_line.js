9aa8e55b20969f34ad746d999234c662da1bf360
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-27 15:11:50 -0500

    [UPD] e_design_pos: Use new eDesignConfiguratorPopup

--- e_design_pos/static/src/components/pos_order_line.js
+++ e_design_pos/static/src/components/pos_order_line.js
@@ -2,9 +2,6 @@ import { patch } from "@web/core/utils/patch";
 import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
 
 patch(PosOrderline.prototype,{
-    setup(vals){
-        super.setup(vals)
-    },
     can_be_merged_with(orderline){
         return super.can_be_merged_with(orderline) && (orderline.design_id?.id === this.design_id?.id )
     },
@@ -12,9 +9,7 @@ patch(PosOrderline.prototype,{
         let data = super.getDisplayData()
         return {
             ...data,
-            design_id: this.design_id?{
-                default_code: this.design_id.default_code
-            }:false
+            design_id: this.design_id?{'default_code':this.design_id.default_code}:false
         }
     }
 })
