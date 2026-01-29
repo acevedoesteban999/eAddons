5c4af7faa5d256390011628e7e6d327429d75bbd
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-28 22:37:53 -0500

    [UPD]

--- e_design_bundle/__manifest__.py
+++ e_design_bundle/__manifest__.py
@@ -2,21 +2,20 @@
 {
     'name': 'eDesignBundle',
     'version': '18.1.0',
-    'summary': """ eDesign Bundle for all eDesign addons""",
+    'summary': "Complete bundle for all eDesign addons",
     'author': 'acevedoesteban999@gmail.com',
     'website': 'https://github.com/acevedoesteban999/eAddons',
-    'category': '',
+    'category': 'Customizations',
     'depends': [
         'e_design',
         'e_pos_mrp',
         'e_design_mrp_pos',
         'e_design_mrp_sale',
         'e_design_pos',
-        
         'e_design_website',
         'e_design_website_tv_catalog',
         'e_design_sale',
-        'e_design_account' ,
+        'e_design_account',
     ],
     'images': [
         'static/description/banner.png',
