f0a937ddd6acbc838254171d8a559ca512b9ff7f
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-30 14:13:53 -0500

    [FIX] e_pos_out_stock: Fix category view. [UPD] e_pos_out_stock: Updata var show_pos_outstock

--- e_pos_out_stock/models/product_template.py
+++ e_pos_out_stock/models/product_template.py
@@ -6,5 +6,5 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    show_pos_outofstock = fields.Boolean("Show products in POS with Out of Stock")
+    show_pos_outstock = fields.Boolean("Show products in POS with Out of Stock")
     
