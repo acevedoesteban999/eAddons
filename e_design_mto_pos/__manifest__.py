03524fd198e0c8d57cea967250f3dddc3455b34d
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-02-03 20:08:59 -0500

    [FIX] e_design_mto_pos

--- e_design_mto_pos/__manifest__.py
+++ e_design_mto_pos/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'eDesignMtoPos',
-    'version': '18.0.0.1.0',
+    'version': '18.0.0.0.1',
     'summary': "Integration eDesign with eMtoPos",
     'description':"Show design icon into eMtoPos Picking Lines",
     'author': 'acevedoesteban999@gmail.com',
     'website': 'https://github.com/acevedoesteban999/eAddons/blob/18.0/e_design_mto_pos',
     'category': 'Customizations',
-    'depends': ['e_mto_pos','e_design_mrp_pos'],
+    'depends': ['e_mto_pos','e_design_pos'],
     'data': [],
     'images': [
         'static/description/banner.png',
--- e_design_mto_pos/models/stock_picking.py
+++ e_design_mto_pos/models/stock_picking.py
@@ -1,5 +1,4 @@
-from odoo import models , api  , fields
-from odoo.osv.expression import AND
+from odoo import models , api
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
     
@@ -7,11 +6,11 @@ class StockPicking(models.Model):
     def read_picking_lines(self, picking_id):
         lines = super().read_picking_lines(picking_id)
         for line in lines:
-            line = self.env['pos.order.line'].browse(line.get('id'))
-            if line.design_id:
+            order_line = self.env['pos.order.line'].browse(line.get('id'))
+            if order_line.design_id:
                 line.update({
                     'design_id':{
-                        'default_code': line.design_id.default_code or '',
+                        'default_code': order_line.design_id.default_code or '',
                     }
                 })
                 
