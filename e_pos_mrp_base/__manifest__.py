fd595562fe184ec586a3c7776a324544d4973323
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-02-06 08:16:54 -0500

    [NEW] e_pos_mrp_base: Base for pos_order's fields

---
+++ e_pos_mrp_base/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ePosMrpBase',
+    'version': '18.0.0.0.0',
+    'summary': "",
+    'description':"",
+    'author': 'acevedoesteban999@gmail.com',
+    'website': 'https://github.com/acevedoesteban999/eAddons/blob/18.0/e_pos_mrp_base',
+    'category': 'Customizations',
+    'depends': ['pos_mrp'],
+    'data': [],
+    'images': [
+        'static/description/banner.png',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ e_pos_mrp_base/models/mrp_production.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import models,fields,_
+
+class MrpProduction(models.Model):
+    
+    _inherit = 'mrp.production'
+    
+    pos_order_id = fields.Many2one('pos.order',"Origin POS",compute="_compute_pos_order")
+    pos_order_line_id = fields.Many2one('pos.order.line',"Origin POS line")
+    
+    def _compute_pos_order(self):
+        for rec in self:
+            rec.pos_order_id = rec.pos_order_line_id.order_id
+            
+    
