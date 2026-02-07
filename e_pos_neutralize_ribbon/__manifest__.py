a90346fb68843539af9aa1305467c3d695da8b3f
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-02-06 20:01:44 -0500

    [UPD]

--- e_pos_neutralize_ribbon/__manifest__.py
+++ e_pos_neutralize_ribbon/__manifest__.py
@@ -2,6 +2,8 @@
     'name': 'ePosNeutralizeRibbon',
     'version': '18.0.0.0.0',
     'depends': ['point_of_sale'],
+    'author': 'acevedoesteban999@gmail.com',
+    'website': 'https://github.com/acevedoesteban999/eAddons/blob/18.0/e_pos_neutralize_ribbon',
     'data': [
         'data/neutralize_pos_data.xml',
     ],
